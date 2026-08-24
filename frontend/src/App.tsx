cc4e251a40b237527bdc424a6648e7a06351d81c
Sahina <sahinakhatun3675@gmail.com>
2026-08-24 17:58:17 +0530

    feat: update application routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,26 +10,42 @@ import DiscoverEvents from "./pages/AttendeePortal/DiscoverEvents";
 import MyTickets from "./pages/AttendeePortal/MyTickets";
 import Schedule from "./pages/AttendeePortal/Schedule";
 import Feedback from "./pages/Feedback/Feedback";
+import VendorLayout from "./pages/VendorPortal/VendorLayout";
+import VendorDashboardHome from "./pages/VendorPortal/VendorDashboardHome";
+import Opportunities from "./pages/VendorPortal/Opportunities";
+import MyAssignments from "./pages/VendorPortal/MyAssignments";
+import Payments from "./pages/VendorPortal/Payments";
+import Reviews from "./pages/VendorPortal/Reviews";
 import { AttendeeProvider } from "./context/AttendeeContext";
+import { VendorProvider } from "./context/VendorContext";
 
 function App() {
   return (
     <BrowserRouter>
       <AttendeeProvider>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/dashboard/*" element={<DashboardApp />} />
-          <Route path="/attendee" element={<AttendeeLayout />}>
-            <Route index element={<AttendeeDashboardHome />} />
-            <Route path="discover" element={<DiscoverEvents />} />
-            <Route path="tickets" element={<MyTickets />} />
-            <Route path="schedule" element={<Schedule />} />
-            <Route path="feedback" element={<Feedback />} />
-          </Route>
-        </Routes>
+        <VendorProvider>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/dashboard/*" element={<DashboardApp />} />
+            <Route path="/attendee" element={<AttendeeLayout />}>
+              <Route index element={<AttendeeDashboardHome />} />
+              <Route path="discover" element={<DiscoverEvents />} />
+              <Route path="tickets" element={<MyTickets />} />
+              <Route path="schedule" element={<Schedule />} />
+              <Route path="feedback" element={<Feedback />} />
+            </Route>
+            <Route path="/vendor" element={<VendorLayout />}>
+              <Route index element={<VendorDashboardHome />} />
+              <Route path="opportunities" element={<Opportunities />} />
+              <Route path="assignments" element={<MyAssignments />} />
+              <Route path="payments" element={<Payments />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+          </Routes>
+        </VendorProvider>
       </AttendeeProvider>
     </BrowserRouter>
   );
